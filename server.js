010da1a280e560bc435309243c54233e07cf9aaf
Nico Escovilla <35411308+nicoEXE@users.noreply.github.com>
2022-07-01 07:42:46 +0800

    update sample

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  
+  socket.on("chat", (...args) => {
+    io2.emit('message', args);
+  });
 });
 
+
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
