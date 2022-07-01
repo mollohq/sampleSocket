a14c4fbf2d0795a5490ccf87671c879527f1e973
Nico Escovilla <35411308+nicoEXE@users.noreply.github.com>
2022-07-01 08:04:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const io = require("socket.io")(server, {
 
 
 io.on('connection', (socket) => {
-  alert('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
   
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  io.emit('con', 'Client connected');
   socket.on("chat", (...args) => {
     io.emit('message', args);
   });
