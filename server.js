8971a514674447fd0d0c94e98a23f0bf76259157
Nico Escovilla <35411308+nicoEXE@users.noreply.github.com>
2022-07-01 08:38:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
   
   socket.on('disconnect', () => console.log('Client disconnected'));
   io.emit('con', "Somebody connected");
-  socket.on("message", (args) => {
+  socket.on("messaged", (args) => {
     io.emit('message', args);
   });
 });
