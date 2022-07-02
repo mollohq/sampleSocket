ab4ce6b2bc0b28fdf8310913162b2ddfb35f0a4d
Nico Escovilla <35411308+nicoEXE@users.noreply.github.com>
2022-07-02 13:45:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,19 @@
 'use strict';
-
 const express = require('express');
 const socketIO = require('socket.io');
-
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-// const io = require("socket.io")(server,{
-//   cors: {
-//     origins: "*:*",
-//     methods: ["GET", "POST"]
-//   }
-// });
-
 const io = require("socket.io")(server,{
   cors: {
-    origin: "http://localhost:3000",
+    origins: "*:*",
     methods: ["GET", "POST"]
   }
 });
-
-
-let count = 0
 io.on('connection', (socket) => {
-  
   socket.on('disconnect', () => console.log('Client disconnected'));
-  
   socket.on('messaged', (args) => {
     io.emit('message', args);
   });
@@ -38,14 +21,4 @@ io.on('connection', (socket) => {
     io.emit('message2', args);
      console.log(args)
   });
-  
-//   setInterval(() => {
-//     count++
-//        socket.emit('nani', count);
-//     }, 1000);
- 
 });
-
-
-
-
