61555d9ab953144d7aedb027806b79cdce69dad9
Bob Jelica <bob@jelica.se>
2023-10-13 10:10:38 +0200

    remove shit

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const INDEX = '/index.html';
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-const io = require("socket.io")(server,{
-  cors: {
-    origins: "*:*",
-    methods: ["GET", "POST"]
-  }
-});
+const io = require("socket.io")(server);
 io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on('messaged', (args) => {
